640c69ff19259eba62291f21cf8e270749d0cdb8
tarunsheoran <118544285+tarunsheoran@users.noreply.github.com>
2023-10-07 21:49:24 +0530

    Add files via upload

---
+++ increment and decrement.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int a =6;
+	cout<<a++<<endl;
+	//6,7
+	cout<<++a<<endl;
+	//8,8
+	cout<<a++<<endl;
+	//8,9
+	cout<<a--<<endl;
+	//9,8
+	cout<<--a<<endl;
+	//7,7
+	cout<<a--<<endl;
+	//7,6
+
+
+}
